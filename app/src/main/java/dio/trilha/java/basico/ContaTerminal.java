ca7d5a2f66c08954f09a6037d44c512d834bfb56
ErickSaraiva <erickc.saraiva@gmail.com>
2025-06-29 23:49:25 -0400

    meu projeto conta banco

---
+++ app/src/main/java/dio/trilha/java/basico/ContaTerminal.java
@@ -0,0 +1,8 @@
+package dio.trilha.java.basico;
+
+public class ContaTerminal {
+
+    public static void main(string[] args) throws Exception {
+        System.out.println("olá mundoooo!!");
+    }
+}
